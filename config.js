5f1b5177117d3fc757327431644b860dd96c617f
ducccbb <106739191+ducccbb@users.noreply.github.com>
2024-02-10 00:00:35 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = '10a4';
+const giftUrl = 'https://www.tiktok.com/@a4.k52.pxb';
+const eventName = 'Chúc Mừng Năm mới';
+const titleCard = 'Tặng 10a4';
+const contentCard = 'Gửi đến tập thể lớp 10A4: Nhân dịp năm mới Giáp Thìn 2024, kính chúc toàn thể lớp 10A4 một năm mới An khang thịnh vượng - Vạn sự như ý!';
+const giftImage = '10a4.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
